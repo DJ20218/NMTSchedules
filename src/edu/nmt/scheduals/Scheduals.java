da3315867c16eefedfb41b7a20246f51038cae46
tunnelcharger <tunnelguy2@gmail.com>
2015-01-16 14:58:44 -0700

    Push Base android code
    pushed base code

---
+++ src/edu/nmt/scheduals/Scheduals.java
@@ -0,0 +1,15 @@
+package edu.nmt.scheduals;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Scheduals extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
