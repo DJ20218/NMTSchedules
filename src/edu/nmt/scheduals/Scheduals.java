51f6c4acd1a199ac204a7978ed094538fe32e2ae
tunnelcharger <tunnelguy2@gmail.com>
2015-01-19 18:05:57 -0700

    Test App
    I have now created a basic text box counter.
    Also, configured a gitignore file.

--- src/edu/nmt/scheduals/Scheduals.java
+++ src/edu/nmt/scheduals/Scheduals.java
@@ -2,14 +2,22 @@ package edu.nmt.scheduals;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
 
 public class Scheduals extends Activity
 {
+    private int count = 0;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    public void countUp(View view){
+	count+=1;
+	TextView texty = (TextView) findViewById(R.id.mytextview);	
+	texty.setText("Hello World: "+count);
     }
 }
